334e15c1ff97120b94ff82b8c718f5a9f3ec66ba
sidneymai02 <sidneymai2010@yahoo.com>
2022-12-14 21:44:40 -0800

    Added feature where time affects score
    Depending on the time, you get a certain amount of points

--- Final Project Game/Assets/Scripts/UI/Timer.cs
+++ Final Project Game/Assets/Scripts/UI/Timer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,11 +11,17 @@ public class Timer : MonoBehaviour
     private float startTime;
     private bool finished = false;
     private PlayerMovement pm;
+    private float t;
+    private string minutes;
+    private string seconds;
+
+    public ScoreTracker timeScore;
     // Start is called before the first frame update
     void Start()
     {
         startTime = Time.time;
         pm = GetComponent<PlayerMovement>();
+        timeScore = FindObjectOfType<ScoreTracker>();
     }
     
     // Update is called once per frame
@@ -24,15 +31,25 @@ public class Timer : MonoBehaviour
             return;
         }
 
-        float t = Time.time - startTime;
-        string minutes = ((int) t / 60).ToString();
-        string seconds = (t % 60).ToString("f2");
+        t = Time.time - startTime;
+        minutes = ((int) t / 60).ToString();
+        seconds = (t % 60).ToString("f2");
 
         timerText.text = "Timer: " + minutes + ":" + seconds;
     }
     public void Finish() {
+        Debug.Log("Done in: " + minutes + " and " + seconds);
         pm.freeze = true;
         finished = true;
         timerText.color = Color.red;
+
+        if (minutes.Equals("0"))
+        {
+            timeScore.score = timeScore.score + 3;
+        }
+        else
+        {
+            timeScore.score = timeScore.score + 1;
+        }
     }
 }
